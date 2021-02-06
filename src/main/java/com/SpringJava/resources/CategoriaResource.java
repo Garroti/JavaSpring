ffaa88c1780b617d96102909d92708095e730caf
Garroti <gabryelmacedo0@gmail.com>
2021-02-06 00:39:17 -0300

    desenvolvendo crud do projeto

--- src/main/java/com/SpringJava/resources/CategoriaResource.java
+++ src/main/java/com/SpringJava/resources/CategoriaResource.java
@@ -1,11 +1,15 @@
 package com.SpringJava.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.SpringJava.domain.Categoria;
 import com.SpringJava.services.CategoriaService;
@@ -18,11 +22,34 @@ public class CategoriaResource {
 	private CategoriaService service;
 	
 	@RequestMapping(value="/{id}", method=RequestMethod.GET)
-	public ResponseEntity<?> find(@PathVariable Integer id) {
+	public ResponseEntity<Categoria> find(@PathVariable Integer id) {
 		
-		Categoria obj = service.buscar(id);
+		Categoria obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
 	
+	}
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Categoria> insert(@RequestBody Categoria obj) {
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder
+					.fromCurrentRequest().path("/{id}")
+					.buildAndExpand(obj.getId()).toUri();
+		
+		return ResponseEntity.created(uri).build();
+	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
+	public ResponseEntity<Categoria> update(@RequestBody Categoria obj, @PathVariable Integer id) {
+		obj.setId(id);
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable Integer id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 	
 }
--- src/main/java/com/SpringJava/services/CategoriaService.java
+++ src/main/java/com/SpringJava/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.SpringJava.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.SpringJava.domain.Categoria;
 import com.SpringJava.repositories.CategoriaRepository;
+import com.SpringJava.services.exceptions.DataIntegrityException;
 import com.SpringJava.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -15,10 +17,29 @@ public class CategoriaService {
 	@Autowired
 	private CategoriaRepository repoCategoria;
 	
-	public Categoria buscar(Integer id) {
+	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repoCategoria.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+	}
+	
+	public Categoria insert(Categoria obj) {
+		obj.setId(null);
+		return repoCategoria.save(obj);
+	}
+	
+	public Categoria update(Categoria obj) {
+		find(obj.getId());
+		return repoCategoria.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repoCategoria.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possivel excluir uma categoria que possui produtos");
+		}
 	}
 
 }
